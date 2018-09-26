e4076e06c05a7aa02366e4137b68c3e20b89296e
intOppong <oppong.lord@gmail.com>
2018-09-26 09:56:11 +0000

    deleted node-modules dir

---
+++ tutorial_projects/node_react_fullstack_web_dev/client/src/components/Landing.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Landing extends Component {
+
+  render() {
+    return (
+      <div style={{ textAlign:'center' }}>
+        <h2>Emaily</h2>
+        <p>Collect feedback from users</p>
+      </div>
+
+    )
+  }
+}
+
+export default Landing;
