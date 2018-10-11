5127cb2c5ede47889ac19535ae757755f6d5ca6a
intOppong <oppong.lord@gmail.com>
2018-10-11 05:00:56 +0000

    navigation: sidebar, sideDrawer, Responsiveness

--- tutorial_projects/burger-builder-react/src/containers/BurgerBuilder/BurgerBuilder.js
+++ tutorial_projects/burger-builder-react/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -85,10 +85,19 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler = () => {
-    const oldState = this.state.purchasing;
     this.setState({
-      purchasing: !oldState
+      purchasing: true
     })
+  }
+
+  purchaseCancelHandler = () => {
+    this.setState({
+      purchasing: false
+    })
+  }
+
+  purchaseContinueHandler = () => {
+    alert('Continue');
   }
 
 
@@ -102,11 +111,15 @@ class BurgerBuilder extends Component {
     return (
       <Aux>
 
-        <Modal show={this.state.purchasing}>
-          <OrderSummary ingredients={this.state.ingredients} />
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            cancelPurchase={this.purchaseCancelHandler}
+            continuePurchase={this.purchaseContinueHandler}
+            price={this.state.totalPrice} />
         </Modal>
 
-        <Burger ingredients={this.state.ingredients}/>
+        <Burger ingredients={this.state.ingredients} />
 
         <BuildControls
           addIngredient={this.addIngredientHandler}
